e5ebde5e147b1df3892ed200c1b329b479e6fe3a
vladostan <vladostan94@gmail.com>
2019-09-18 18:08:56 +0300

    mp4 to png

--- mp4_to_png.py
+++ mp4_to_png.py
@@ -9,12 +9,12 @@ from tqdm import tqdm
 folder ='../../../colddata/datasets/footbolchik/mp4/'
 mp4s = [f for f in glob(folder + '*.mp4', recursive=True)]
 
-for i, mp4 in tqdm(enumerate(mp4s)):
+for mp4 in tqdm(mp4s):
     vidcap = cv2.VideoCapture(mp4)
     success,image = vidcap.read()
     count = 0
     
-    newfolder = '../../../colddata/datasets/footbolchik/png/00{}'.format(i+1)
+    newfolder = '../../../colddata/datasets/footbolchik/png/{}'.format(mp4.split('.mp4')[0][-3:])
     
     if not os.path.exists(newfolder):
         os.makedirs(newfolder)
