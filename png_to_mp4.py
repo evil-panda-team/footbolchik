2840b00cda3be3c6e19c3c408d9df88dc080f571
Vladislav Ostankovich <vladostan94@gmail.com>
2019-09-19 21:50:16 +0300

    png to mp4 converter

---
+++ png_to_mp4.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import cv2
+from glob import glob
+from tqdm import tqdm
+
+# In[]:
+folder ='../mmdetection/results/'
+size = (1920, 1080)
+
+jpgs = [f for f in glob(folder + '*.jpg', recursive=True)]
+jpgs.sort()
+
+out = cv2.VideoWriter('crop_p1080_detections.mp4', cv2.VideoWriter_fourcc(*'XVID'), 24, size)
+
+for jpg in tqdm(jpgs):
+    img = cv2.imread(jpg)
+    out.write(img)
+out.release()
+
+# In[]:
+
+
+
+
+# In[]:
+
+
+
+
+# In[]:
+
+
+
