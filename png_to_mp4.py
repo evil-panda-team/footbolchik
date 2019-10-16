dc4986bcf4994b2f5eb0a62862aa8ba48105e9eb
vladostan <vladostan94@gmail.com>
2019-10-16 18:54:15 +0300

    Added inference from video and centroids saver to pickle

--- png_to_mp4.py
+++ png_to_mp4.py
@@ -11,7 +11,7 @@ size = (1920, 1080)
 jpgs = [f for f in glob(folder + '*.jpg', recursive=True)]
 jpgs.sort()
 
-out = cv2.VideoWriter('crop_p1080_detections.mp4', cv2.VideoWriter_fourcc(*'XVID'), 24, size)
+out = cv2.VideoWriter('005_detections.mp4', cv2.VideoWriter_fourcc(*'XVID'), 24, size)
 
 for jpg in tqdm(jpgs):
     img = cv2.imread(jpg)
