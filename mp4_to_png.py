35c27aaba23598457ee2d9e75d0d6dc8fa5fc7fd
vladostan <vladostan94@gmail.com>
2019-11-09 14:43:26 +0300

    Updated mp4 to png converter with fps

--- mp4_to_png.py
+++ mp4_to_png.py
@@ -6,24 +6,30 @@ import os
 from tqdm import tqdm
 
 # In[]:
-folder ='../../../colddata/datasets/footbolchik/mp4/'
+#folder ='../../../colddata/datasets/footbolchik/mp4/'
+folder ='../../datasets/footbolchik/mp4/'
 mp4s = [f for f in glob(folder + '*.mp4', recursive=True)]
+mp4s.sort()
+
+skip = 2
+start_sec = 10
 
 for mp4 in tqdm(mp4s):
     vidcap = cv2.VideoCapture(mp4)
+    fps = vidcap.get(cv2.CAP_PROP_FPS)
     success,image = vidcap.read()
     count = 0
     
-    newfolder = '../../../colddata/datasets/footbolchik/png/{}'.format(mp4.split('.mp4')[0][-3:])
+    newfolder = '../../datasets/footbolchik/png/{}'.format(mp4.split('.mp4')[0][-3:])
     
     if not os.path.exists(newfolder):
         os.makedirs(newfolder)
     
     while success:
-      cv2.imwrite("{}/{}.png".format(newfolder,count), image)     # save frame as JPEG file      
-      success,image = vidcap.read()
-#      print('Read a new frame: ', success)
-      count += 1
+        if not count%skip and count >= start_sec*fps:
+            cv2.imwrite("{}/{}.png".format(newfolder,count), image)     # save frame as JPEG file      
+        success,image = vidcap.read()
+        count += 1
 
 # In[]:
 
